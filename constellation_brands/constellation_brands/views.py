a595cc05788124e5f9ee273fe93325739e5ab7a2
Ryan <rfriel@rezjam.com>
2021-02-25 15:16:02 -0500

    Added a home view for main project.

---
+++ constellation_brands/constellation_brands/views.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'constellation_brands/home.html'
