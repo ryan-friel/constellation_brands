792d8e05bcb1f2be24f5b50e157c19a914bc22b6
Ryan <rfriel@rezjam.com>
2021-02-25 14:00:28 -0500

    Created models and ran migrations.

--- constellation_brands/reservations/models.py
+++ constellation_brands/reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Import winery. One to Many relationship from Resveration to Winery.
+from wineries.models import Winery
+
+class Reserveration(models.Model):
+
+    # Fields
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resveration = models.ForeignKey(Winery, on_delete=models.CASCADE)
+    reservation_time = models.DateField()
+
+    def __str__(self):
+        return f"self.user at self.resveration.name"
--- constellation_brands/wineries/models.py
+++ constellation_brands/wineries/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Winery(models.Model):
+
+    # Fields
+    name = models.CharField(max_length=128)
+    slogan = models.CharField(max_length=128)
+    description = models.TextField()
+    website = models.URLField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name.title()}"
